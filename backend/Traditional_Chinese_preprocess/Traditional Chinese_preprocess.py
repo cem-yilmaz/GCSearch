05b849ca35b338802fb5ac461f1afebc8f133f1b
yuhsin-k9012 <hebe456789@gmail.com>
2025-02-20 18:21:26 +0100

    updated Traditional Chinese_preprocess.py

--- backend/Traditional_Chinese_preprocess/Traditional Chinese_preprocess.py
+++ backend/Traditional_Chinese_preprocess/Traditional Chinese_preprocess.py
@@ -1,20 +1,15 @@
 import pandas as pd
 import jieba
 import jieba.analyse
+import re
 from sentence_transformers import SentenceTransformer
 from symspellpy import SymSpell, Verbosity
 
 # stopwords
-def load_stopwords(filepath="stop_words.txt"):
+def load_stopwords(filepath="jeiba/stop_words.txt"):
     with open(filepath, "r", encoding="utf-8") as f:
         stopwords = set(f.read().splitlines())
     return stopwords
-
-# SymSpell (spelling correction)
-def load_symspell(dictionary_path="bigram_dictionary.txt"):
-    sym_spell = SymSpell(max_dictionary_edit_distance=2)
-    sym_spell.load_dictionary(dictionary_path, term_index=0, count_index=1)
-    return sym_spell
 
 # correct wrong spelling
 def correct_spelling(text, sym_spell):
@@ -28,13 +23,13 @@ def correct_spelling(text, sym_spell):
     return "".join(corrected_words)
 
 # initialize jieba
-jieba.set_dictionary("dict.txt.big")  # Traditional Chinese dic
-jieba.analyse.set_idf_path("idf.txt.big")  # TF-IDF use
+jieba.set_dictionary("jeiba/dict.txt.big")  # Traditional Chinese dic
+jieba.analyse.set_idf_path("jeiba/idf.txt.big")  # TF-IDF use
 
 # jieba tokenizer for search engine use
-def jieba_tokenizer(text, stopwords, sym_spell):
+def jieba_tokenizer(text, stopwords):
     # correct wrong spelling
-    text = correct_spelling(text, sym_spell)
+    # text = correct_spelling(text, sym_spell)
 
     # keep only chinese words
     # text = re.sub(r'[^\u4e00-\u9fff]', ' ', text)
@@ -56,59 +51,82 @@ def extract_tfidf_keywords(text, stopwords, topK=5):
     
     return " ".join(filtered_keywords)
 
+# BERT
+def compute_sentence_embedding(texts):
+    model = SentenceTransformer('paraphrase-multilingual-MiniLM-L12-v2')
+    embeddings = model.encode(texts)
+    return embeddings
 
 # read csv file
+# def read_csv_file(filepath):
+#     df = pd.read_csv(filepath)
+
+#     if "message" not in df.columns or "is_media" not in df.columns:
+#         raise ValueError("CSV should have included 'message' and 'is_media' col")
+
+#     text_df = df[df["is_media"] == False][["message"]].dropna().astype(str)
+
+#     return text_df
 def read_csv_file(filepath):
     df = pd.read_csv(filepath)
-
-    if "message" not in df.columns or "is_media" not in df.columns:
-        raise ValueError("CSV should have included 'message' and 'is_media' col")
-
-    text_df = df[df["is_media"] == False][["message"]].dropna().astype(str)
-
+    
+    # col exists or not
+    required_cols = ["doc_id", "message", "is_media"]
+    for col in required_cols:
+        if col not in df.columns:
+            raise ValueError(f"CSV must include '{col}' column")
+    
+    # is_media=False
+    filtered_df = df[df["is_media"].astype(str).str.upper() == "FALSE"]
+    
+    # keep doc_id and message
+    text_df = filtered_df[["doc_id", "message"]].dropna().astype(str)
     return text_df
 
 # pre-process
-def preprocess_messages(df, stopwords, sym_spell):
+def preprocess_messages(df, stopwords):
+    doc_ids = df["doc_id"]
     chat_messages = df["message"].dropna().astype(str)
 
     # token
-    processed_tokens = chat_messages.apply(lambda text: jieba_tokenizer(text, stopwords, sym_spell))
+    processed_tokens = chat_messages.apply(lambda text: jieba_tokenizer(text, stopwords))
 
     # TF-IDF
     tfidf_keywords = chat_messages.apply(lambda text: extract_tfidf_keywords(text, stopwords, topK=5))
 
+    # BERT
+    embeddings = compute_sentence_embedding(processed_tokens.tolist())
 
     # create DataFrame
     processed_df = pd.DataFrame({
+        "doc_id":doc_ids,
         "original_message": chat_messages,
         "processed_tokens": processed_tokens,
-        "tfidf_keywords": tfidf_keywords  
+        "tfidf_keywords": tfidf_keywords,
+        "embedding_vector": list(embeddings)  
     })
 
     return processed_df
 
-input_csv_path = "chatlog.csv"  # input csv
-output_csv_path = "tra_chinese_preprocess.csv"  # output csv
+input_csv_path = "jeiba/chatlog_latestneeeew.csv"  # input csv
+#input_csv_path ='chatlog_latestneeeew1.csv'
+output_csv_path = "jeiba/processed_chatlog.csv"  # output csv
 
 # read csv
-df = read_csv_file(input_csv_path)
 print("read csv file")
+df = read_csv_file(input_csv_path)
 
-# stopwords
-stopwords = load_stopwords("stop_words.txt")
 print("loading stopwords")
-
-# spelling correction model
-sym_spell = load_symspell("bigram_dictionary.txt")
-print("loading spelling correction")
+# stopwords
+stopwords = load_stopwords("jeiba/stop_words.txt")
 
 # pre-processing
-processed_chat_df = preprocess_messages(df, stopwords, sym_spell)
-print("pre-processing messages")
+print("pre-processing text messages")
+processed_chat_df = preprocess_messages(df, stopwords)
+print("completed pre-processing messages")
 
 # save file
+print("saving into output csv file")
 processed_chat_df.to_csv(output_csv_path, index=False, encoding="utf-8-sig")
-print("save into output csv file")
 
 print("Completed")
